1f75ae7d6207727592463fe75754a595d3899138
Jonathan Swap <js7jx@virginia.edu>
2024-06-28 11:05:42 -0400

    montecarlo

---
+++ montecarlo/montecarlo.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+
+
+
+class Die():
+    def __init__(self, sides):
+        weights = np.ones(sides)
+        symbols = [i for i in range(0:len(sides))]    
+
+
+
+        def roll_more():
+            pass
+
+
+
+
+
+class Game():
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+class Analyzer():
+    def __init__(self):
+        pass
